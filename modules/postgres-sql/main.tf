eb8844b948938c359b1b0d25f6d2a45acd8feffc
satwick <satwickcherukuri@gmail.com>
2025-06-30 21:31:22 +0530

    Update main.tf

--- modules/postgres-sql/main.tf
+++ modules/postgres-sql/main.tf
@@ -1,3 +1,39 @@
-# Sample Terraform configuration for postgres-sql
+####################
+# Provider section #
+####################
 
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Resources section #
+#####################
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/postgresql_flexible_server
+resource "azurerm_postgresql_flexible_server" "psql" {
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tags                          = var.tags
+  delegated_subnet_id           = var.subnet_id
+  private_dns_zone_id           = var.private_dns_zone_id
+  administrator_login           = var.username
+  administrator_password        = var.password
+  sku_name                      = var.sku_name
+  version                       = var.db_version
+  storage_mb                    = var.storage_mb
+  auto_grow_enabled             = var.auto_grow_enabled
+  backup_retention_days         = var.backup_retention_days
+  zone                          = var.zone
+  depends_on                    = [var.psqlflex_depends_on]
+ }
