82db55157cee10ab2d5d9c6855918a8c0a870183
satwick <satwickcherukuri@gmail.com>
2025-06-30 22:23:19 +0530

    Update outputs.tf

--- modules/route-table/outputs.tf
+++ modules/route-table/outputs.tf
@@ -1,4 +1,4 @@
-output "example" {
-  description = "Example output for route-table"
-  value       = var.example
+output "route_table_id" {
+  description = "The ID of the deployed route table"
+  value       = azurerm_route_table.table.id
 }
