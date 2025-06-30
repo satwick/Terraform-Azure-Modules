1281c949eaebe486fe691f23787d6fc09aadb8c4
satwick <satwickcherukuri@gmail.com>
2025-06-30 21:29:47 +0530

    Update outputs.tf

--- modules/key-vault/outputs.tf
+++ modules/key-vault/outputs.tf
@@ -1,4 +1,4 @@
-output "example" {
-  description = "Example output for key-vault"
-  value       = var.example
+output "id" {
+  description = "ID of the Key-vault"
+  value       = azurerm_key_vault.key-vault.id
 }
