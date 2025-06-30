4a8e16b035418dc6822ea22153760efc245e54de
satwick <satwickcherukuri@gmail.com>
2025-06-30 21:27:07 +0530

    Update main.tf

--- modules/azure-sql-server/main.tf
+++ modules/azure-sql-server/main.tf
@@ -1,3 +1,52 @@
-# Sample Terraform configuration for azure-sql-server
+####################
+# Provider section #
+####################
 
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Resources section #
+#####################
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/mssql_server
+resource "azurerm_mssql_server" "sql_server" {
+  name                         = var.sql_server_name
+  resource_group_name          = var.resource_group_name
+  location                     = var.location
+  version                      = var.sql_server_version
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  minimum_tls_version          = var.minimum_tls_version
+  depends_on                   = [var.sql_depends_on]
+}
+
+resource "azurerm_private_endpoint" "private_endpoint" {
+  for_each = var.private_endpoint
+
+  name                = each.key
+  resource_group_name = azurerm_mssql_server.sql_server.resource_group_name
+  location            = azurerm_mssql_server.sql_server.location
+  subnet_id           = each.value.subnet_id
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  private_service_connection {
+    name                           = "${each.key}psc"
+    private_connection_resource_id = azurerm_mssql_server.sql_server.id
+    is_manual_connection           = false
+    subresource_names              = ["SQLserver"]
+  }
+}
