1db30f2f7390710fa587d7f81f14a1456144f82b
satwick <satwickcherukuri@gmail.com>
2025-06-30 22:22:37 +0530

    Update main.tf

--- modules/route-table/main.tf
+++ modules/route-table/main.tf
@@ -1,3 +1,39 @@
-# Sample Terraform configuration for route-table
+####################
+# Provider section #
+####################
 
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Resources section #
+#####################
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/route_table
+resource "azurerm_route_table" "table" {
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tags                          = var.tags
+  disable_bgp_route_propagation = var.disable_bgp_route_propagation
+
+  dynamic "route" {
+    for_each = var.route_table_routes
+    content {
+      name                   = route.value["name"]
+      address_prefix         = route.value["address_prefix"]
+      next_hop_type          = route.value["next_hop_type"]
+      next_hop_in_ip_address = route.value["next_hop_in_ip_address"]
+    }
+  }
+}
