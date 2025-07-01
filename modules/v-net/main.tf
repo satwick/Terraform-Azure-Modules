b4c6d6483aa3df477e0b8faf2c7be3fe8b81f5cb
satwick <satwickcherukuri@gmail.com>
2025-07-01 10:18:15 +0530

    Update main.tf

--- modules/v-net/main.tf
+++ modules/v-net/main.tf
@@ -1,3 +1,53 @@
-# Sample Terraform configuration for v-net
+####################
+# Provider section #
+####################
 
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Resources section #
+#####################
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+  address_space       = var.address_space
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+resource "azurerm_subnet" "subnets" {
+  for_each = var.subnets
+
+  name                                          = each.key
+  resource_group_name                           = var.resource_group_name
+  virtual_network_name                          = azurerm_virtual_network.vnet.name
+  address_prefixes                              = each.value.address_prefix
+  private_link_service_network_policies_enabled = each.value.private_link_service_network_policies_enabled
+  private_endpoint_network_policies_enabled     = each.value.private_endpoint_network_policies_enabled
+  service_endpoints                             = each.value.service_endpoints
+
+  dynamic "delegation" {
+    for_each = each.value.delegation
+    content {
+      name = "delegation"
+      service_delegation {
+        name    = delegation.value.name
+        actions = delegation.value.actions
+      }
+    }
+  }
+}
