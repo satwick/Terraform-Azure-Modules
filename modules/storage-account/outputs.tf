7305dec0af32fed7257be599d9cbe3dd122d867d
satwick <satwickcherukuri@gmail.com>
2025-07-01 10:16:41 +0530

    Update outputs.tf

--- modules/storage-account/outputs.tf
+++ modules/storage-account/outputs.tf
@@ -1,4 +1,24 @@
-output "example" {
-  description = "Example output for storage-account"
-  value       = var.example
+output "storage_account_name" {
+  description = "The name of the deployed storage account"
+  value       = azurerm_storage_account.storage.name
+}
+
+output "storage_containers" {
+  description = "An array consisting of all storage container names with their IDs"
+  value       = { for k, v in azurerm_storage_container.blob : k => v }
+}
+
+output "access_key" {
+  description = "The primary access key to access the storage through the unique URL"
+  value       = azurerm_storage_account.storage.primary_access_key
+}
+
+output "primary_blob_endpoint" {
+  description = "The primary access point or URL for interacting with a Blob Storage service"
+  value       = azurerm_storage_account.storage.primary_blob_endpoint
+}
+
+output "id" {
+  description = "ID of the storage account"
+  value       = azurerm_storage_account.storage.id
 }
