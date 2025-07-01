5735ba3b79d0444e375fff519903bb9c4929280c
satwick <satwickcherukuri@gmail.com>
2025-07-01 10:23:49 +0530

    Update main.tf

--- modules/vm-windows/main.tf
+++ modules/vm-windows/main.tf
@@ -1,3 +1,64 @@
-# Sample Terraform configuration for vm-windows
+####################
+# Provider section #
+####################
 
-resource "null_resource" "example" {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.98.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+#####################
+# Resources section #
+#####################
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/windows_virtual_machine
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tags                = var.tags
+
+  size           = var.size
+  admin_username = var.username
+  admin_password = var.password
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  os_disk {
+    name                 = "${var.name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+
+  depends_on = [var.vmwindows_depends_on]
+}
