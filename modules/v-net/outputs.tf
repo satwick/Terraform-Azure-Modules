dbe6e8120e383a728128469cd68080d82d341765
satwick <satwickcherukuri@gmail.com>
2025-07-01 10:19:20 +0530

    Update outputs.tf

--- modules/v-net/outputs.tf
+++ modules/v-net/outputs.tf
@@ -1,4 +1,16 @@
-output "example" {
-  description = "Example output for v-net"
-  value       = var.example
+output "vnet_id" {
+  description = "The ID of the deployed VNET"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+output "vnet_name" {
+  description = "The name of the deployed VNET"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "subnet_ids" {
+  description = "An array consisting of all subnets names with their IDs"
+  value = {
+    for key, subnet in azurerm_subnet.subnets : key => subnet.id
+  }
 }
